95cfa29cc06976551818a06a89f415d9ce66669a
srinivas-choudarapu <srinuchoudarapu@gmail.com>
2026-02-16 21:52:15 +0530

    supabase

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const cors=require('cors');
 const app=express();
-const port=process.env.PORT || 5000;
 require('dotenv').config();
 const supabase=require('./supabase.js');
 
 
+const port=process.env.PORT ;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
